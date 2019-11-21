e298995aa858d17a4b980eba1c353087d8b8b0bf
KimYoonJin <95yjkim95@gmail.com>
2019-11-21 14:12:21 +0900

    DFS

---
+++ 프로그래머스 코딩테스트연습/BFS,DFS/level3_단어변환.cpp
@@ -0,0 +1,56 @@
+#include <string>
+#include <vector>
+#include<iostream>
+#include<queue>
+
+using namespace std;
+
+bool check[50] = { false };	//꼭 추가해서 불필요한 시간 줄이기
+
+int solution(string begin, string target, vector<string> words) {
+
+	int answer = 0;
+	int len = begin.size();
+	
+	queue<pair<string,int>> q;
+	q.push(make_pair(begin,0));
+
+	while (!q.empty()) {
+		string cur_s = q.front().first;
+		int cur_i = q.front().second;
+
+		q.pop();
+
+		if (target == cur_s) {
+			answer = cur_i;
+			break;
+		}
+		
+		//한 알파벳만 다른 경우 변환
+		for (int w = 0; w<words.size(); ++w) {
+			string word = words[w];
+			int dif = 0;
+			for (int i = 0; i < len; i++) {
+				if (check[w] != true) {
+					if (word[i] != cur_s[i]) {
+						dif++;
+					}
+				}
+			}
+			if (dif == 1) {
+				check[w] = true;	
+				q.push(make_pair(word, cur_i + 1));
+				cout << word << " " << cur_i + 1 << endl;
+			}
+		}
+	}
+
+	return answer;
+}
+
+int main() {
+	string begin = "hit", target = "cog";
+	vector<string> words={ "hot", "dot", "dog", "lot", "log", "cog" };
+	int answer = solution(begin,target,words);
+	cout << answer;
+}
