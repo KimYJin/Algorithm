d9c088598d6476f803477c4b739f4dc9535ec28f
KimYoonJin <95yjkim95@gmail.com>
2020-01-12 23:59:24 +0900

    다이나믹 프로그래밍-N으로 표현

---
+++ 프로그래머스 코딩테스트연습/DP/N으로 표현.cpp
@@ -0,0 +1,28 @@
+#include <string>
+#include <vector>
+#include<iostream>
+
+using namespace std;
+
+/*
+N과 괄호, 사칙연산만 사용해서 number를 표현 할 수 있는
+방법 중 N 사용횟수의 최솟값을 return 
+*/
+int solution(int N, int number) {
+	int answer = 0;
+
+
+
+	return answer;
+}
+
+
+int main(void) {
+
+	int N = 5; int number = 12;
+
+	int answer = solution(N, number);
+	cout << answer;
+
+	return 0;
+}
